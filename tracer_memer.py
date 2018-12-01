a81cb5dfa8faafe045b24e4685b2f7ea050136a6
neilgolatkar <45501680+neilgolatkar@users.noreply.github.com>
2018-11-30 23:15:05 -0500

    Update tracer_memer.py

--- tracer_memer.py
+++ tracer_memer.py
@@ -1,32 +1,21 @@
 import praw
 import re
-"""import random
-import time
-import os"""
 
-meme_words = ["tracer"]
 
-reddit = praw.Reddit(client_id='lEu29A23e5Sq1A',
-                     client_secret='vU-XixIIg4UBTY22N0HcTHMuhNY', password='pokemon10',
-                     user_agent='Tracer_Bot 1.0', username='ANAL_DRAWSTRINGS')
+meme_words = [""]
+
+"""Input your own credentials, client secret, and client id here"""
+reddit = praw.Reddit(client_id='Client ID',
+                     client_secret='Client Secret', password='Password',
+                     user_agent='Your User Agent', username='Insert Your Username Here')
 
 print(reddit.user.me())
-"""if not os.path.isfile("posts_replied_to.txt"):
-    posts_replied_to = []
 
-# If we have run the code before, load the list of posts we have replied to
-else:
-    # Read the file into a list and remove any empty values
-    with open("posts_replied_to.txt", "r") as f:
-        posts_replied_to = f.read()
-        posts_replied_to = posts_replied_to.split("\n")
-        posts_replied_to = list(filter(None, posts_replied_to))
-        """
 
-subreddit = reddit.subreddit("Overwatch")
+subreddit = reddit.subreddit("Subreddit name")
 for comment in subreddit.stream.comments():
     print(comment.body.encode("utf-8"))
-    if re.search("tracer", comment.body):
-        tracer_reply = "I'm already Tracer!"
+    if re.search("Comment to search for", comment.body):
+        tracer_reply = "Your comment here"
         comment.reply(tracer_reply)
         print(tracer_reply)
